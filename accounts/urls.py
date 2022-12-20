23f3255d5aaf58a8c6c62f9507005eb51f56b2e5
johnpeterkimeu <johnpeterwambua@gmail.com>
2022-12-20 11:53:03 +0300

    Added my project

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,8 @@ from .  import views
 
 urlpatterns = [
     path('',views.home, name= 'index'),
-    path('',views.shop, name= 'shop')
+    path('',views.shop, name= 'SHOP'),
+    path('', views.Product, name = 'PRODUCTDETAILS'),
+    path('',views.cart, name= 'CART'),
+    path('', views.checkout, name= 'CHECKOUT')
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -9,3 +9,12 @@ def home (request):
 
 def shop (request):
     return render(request, 'shop.html')
+
+def Product(request):
+    return render(request, 'product-details.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def cart(request):
+    return render(request, 'cart.html')
